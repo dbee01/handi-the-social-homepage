9ca74834349f46588bb9754fa69636765ba6bcc9
robbin hood <61830799+dbee01@users.noreply.github.com>
2026-05-05 03:48:25 +0100

    Fix import path for weather model

--- modules/weather/weather.module.js
+++ modules/weather/weather.module.js
@@ -1,2 +1,2 @@
-import M from "./weather.model.js";import V from "./weather.view.js";import C from "./weather.controller.js";
+import M from "./weather.module.js";import V from "./weather.view.js";import C from "./weather.controller.js";
 export default c=>new C(new M(),new V(c)).init();
