e26b5c1561c6ad941e32443bc9df4eef2b6a2b2b
robbin hood <61830799+dbee01@users.noreply.github.com>
2026-05-05 09:07:34 +0100

    Add error handling for container in weather module
    Refactor weather module to include error handling for container initialization.

--- modules/weather/weather.module.js
+++ modules/weather/weather.module.js
@@ -1,2 +1,10 @@
-import M from "./weather.model.js";import V from "./weather.view.js";import C from "./weather.controller.js";
-export default c=>new C(new M(),new V(c)).init();
+import M from "./weather.model.js";
+import V from "./weather.view.js";
+import C from "./weather.controller.js";
+
+export default (c) => {
+  if (!c) throw new Error("Container is required");
+  const controller = new C(new M(), new V(c));
+  controller.init();
+  return controller;
+};
