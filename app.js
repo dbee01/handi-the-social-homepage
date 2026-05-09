f3594d8fd3a90a29b5a3bfca56612609fbbf701a
robbin hood <61830799+dbee01@users.noreply.github.com>
2026-05-09 22:11:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import initSpotify from "./modules/spotify/spotify.module.js";  // Add this
 import initEmergency from "./modules/emergency/emergency.module.js";  // Add this
 import initBus from "./modules/bus/bus.module.js"; 
 import initGallery from "./modules/gallery/gallery.module.js";  
-import initRadio from ".modules/radio/radio.module.js";
+import initRadio from "./modules/radio/radio.module.js";
 import initFriendlyPhone from "./modules/friendly-phone/friendly-phone.module.js";  // Add thi
 import initLayoutSystem from "./modules/ui/layout-system.js";
 
