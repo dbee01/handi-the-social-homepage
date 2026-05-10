90ca858c5db1b7f03b0dcf6fa04304daeb896dab
robbin hood <61830799+dbee01@users.noreply.github.com>
2026-05-10 14:40:03 +0100

    Update local-player.module.js

--- modules/local-player/local-player.module.js
+++ modules/local-player/local-player.module.js
@@ -112,7 +112,7 @@ export default async function initLocalPlayer(container) {
   // Panel Title
   const panelTitle = document.createElement("div");
   panelTitle.className = "panel-title";
-  panelTitle.innerHTML = '<i class="fa-solid fa-music"></i> Local Music Player';
+  panelTitle.innerHTML = '<i class="fa-solid fa-music"></i> Music Player';
   container.appendChild(panelTitle);
 
   const playerContainer = document.createElement("div");
