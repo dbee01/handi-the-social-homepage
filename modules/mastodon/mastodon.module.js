e9c9c42bb733fed73ef7f7305d23f1b6826d2cfb
daz <true.cork.rebel@proton.me>
2026-06-24 02:10:19 +0100

    mobile fixes

--- modules/mastodon/mastodon.module.js
+++ modules/mastodon/mastodon.module.js
@@ -13,7 +13,7 @@ export default async function initMastodon(container) {
   // Title
   const title = document.createElement("div");
   title.className = "panel-title";
-  title.innerHTML = '<i class="fa-brands fa-mastodon"></i> SOCIAL MEDIA';
+  title.innerHTML = '<i class="fa-brands fa-mastodon"></i> SOCIAL';
   container.appendChild(title);
 
   // Content wrapper
