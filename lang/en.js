24d17264e5ab71130f7ab5a46201524a0c5fd61e
daz <true.cork.rebel@proton.me>
2026-09-04 09:40:15 +0100

    branding: unify 'Build Your Own Handi-homepage' link label

--- lang/en.js
+++ lang/en.js
@@ -461,7 +461,7 @@ var LANG = {
 
   // Footer
   d_customizeDashboard: "Customize Dashboard",
-  d_buildOwnPack: "Build Your Own Handi-Site",
+  d_buildOwnPack: "Build Your Own Handi-homepage",
   d_freeTrial: "✨ Try Premium",
 
   // Search placeholder
