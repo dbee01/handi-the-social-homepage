c1e01ab4363d34c5de3e9e8e79e41a92eefe3384
daz <true.cork.rebel@proton.me>
2026-09-17 16:34:08 +0100

    cork: use handi-cork-ss.png as wide manifest screenshot; precache + sw ple-v37

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "ple-v36";
+const CACHE = "ple-v37";
 
 // Manual bump per deploy (you deploy from your working copy, not git). Always
 // raise CACHE before deploying so browsers pick up a changed service worker.
@@ -69,6 +69,7 @@ const PRECACHE = [
   "/images/icons/cork/cork-maskable-192x192.png",
   "/images/icons/cork/cork-maskable-512x512.png",
   "/images/icons/cork/cork-letters-512x512.png",
+  "/images/icons/cork/handi-cork-ss.png",
 ];
 
 // Brand used for notifications until the page tells us which brand is active.
